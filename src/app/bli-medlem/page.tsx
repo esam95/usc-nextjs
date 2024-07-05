6d1d89ad27b9a757f59dd442c7477c293178e88d
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-05 02:07:27 +0200

    feat: create initial form for bli-medlem

--- src/app/bli-medlem/page.tsx
+++ src/app/bli-medlem/page.tsx
@@ -1,26 +1,166 @@
 'use client';
-import { useState } from 'react';
+
+import {
+  Form,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormDescription,
+  FormMessage,
+  FormField,
+} from '@/components/shadcn/form';
+import { Input } from '@/components/shadcn/input';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Button } from '@/components/shadcn/button';
+import { Checkbox } from '@/components/shadcn/checkbox';
+import { RadioGroup, RadioGroupItem } from '@/components/shadcn/radio-group';
+
+const formSchema = z.object({
+  name: z.string().min(2),
+  emailAddress: z.string().email(),
+  adress: z.string().optional(),
+  postalCode: z.string().optional(),
+  personnumber: z.string().min(10).optional(),
+  telephone: z
+    .string()
+    .min(1, 'Please enter a telephone number')
+    .refine((value) => !isNaN(Number(value)), {
+      message: 'Använd ett giltigt format för telefonnummer',
+    }),
+  gender: z.enum(['man', 'woman', '']).optional(),
+});
 
 function BecomeMember() {
-  const postEmail = async () => {
-    const response = await fetch('/api/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-        email: '', // Enter the email you want to send to. This is supposed to be generic later.
-        firstName: '', // Same for this one
-      }),
-    });
-    const data = await response.json();
-    console.log('data :', data);
-  };
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+      emailAddress: '',
+      adress: '',
+      postalCode: '',
+      personnumber: '',
+      telephone: '',
+      gender: 'man',
+    },
+  });
 
+  const handleSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log('values', { values });
+  };
   return (
-    <div className="flex flex-col items-center justify-center my-auto">
-      <h1>Sign the contract</h1>
-      <button className="bg-slate-300 w-24 h-10 rounded-sm" onClick={postEmail}>
-        Bli Medlem
-      </button>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form {...form}>
+        <form className="flex flex-col gap-4 max-w-md w-full" onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            name="name"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-md">För-och efternamn</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fyll i ditt namn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="emailAddress"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-md">E-post</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fyll i din e-post adress" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="telephone"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-md">Telefon</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fyll i ditt telefonnummer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="adress"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-md">Adress</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fyll i din adress" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="postalCode"
+            control={form.control}
+            render={({ field }) => {
+              return (
+                <FormItem>
+                  <FormLabel className="text-md">Postnummer</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Fyll i ditt postnummer" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              );
+            }}
+          />
+          <FormField
+            name="gender"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-md">Kön</FormLabel>
+                <FormControl>
+                  <RadioGroup value={field.value} onValueChange={field.onChange} className="flex gap-6">
+                    <div className="flex gap-3">
+                      <RadioGroupItem value="man" id="man" />
+                      <label htmlFor="man" className="text-sm font-medium leading-none">
+                        Man
+                      </label>
+                    </div>
+                    <div className="flex gap-3">
+                      <RadioGroupItem value="woman" id="woman" />
+                      <label htmlFor="woman" className="text-sm font-medium leading-none">
+                        Kvinna
+                      </label>
+                    </div>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Skicka
+          </Button>
+        </form>
+      </Form>
+    </main>
   );
 }
 
