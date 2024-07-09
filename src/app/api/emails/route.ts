d91b2042a020136ce522e781a303c592272851fc
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-09 14:08:36 +0200

    feat: connect form with server and send email

--- src/app/api/emails/route.ts
+++ src/app/api/emails/route.ts
@@ -1,19 +1,57 @@
 import { Resend } from 'resend';
-import SendEmail from '@/app/emails/sendEmail';
+import EmailTemplate from '@/app/emails/EmailTemplate';
 import { NextResponse } from 'next/server';
 
 export async function POST(request: Request) {
   const resend = new Resend(process.env.RESEND_API_KEY);
-  const { name, email, address } = await request.json();
+  const formData = await request.json();
+
+  // Extract specific fields you need for sending the email
+  const {
+    name,
+    emailAddress,
+    address,
+    postalCode,
+    personnumber,
+    telephone,
+    gender,
+    sports,
+    diseases,
+    trainingFrequency,
+    discount,
+    comments,
+    guardianName,
+    guardianTelephone,
+    friendReferal,
+    friendsName,
+  } = formData;
 
   try {
     await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>', // This is just a testing email, remove later
-      to: email,
+      to: emailAddress,
       subject: 'Test email',
-      react: SendEmail({ name, address }),
+      react: EmailTemplate({
+        name,
+        address,
+        guardianName,
+        guardianTelephone,
+        friendReferal,
+        friendsName,
+        diseases,
+        trainingFrequency,
+        discount,
+        comments,
+        telephone,
+        gender,
+        postalCode,
+        personnumber,
+        emailAddress,
+        sports,
+      }),
     });
 
+    console.log('data', formData);
     return NextResponse.json({
       success: true,
       message: 'Email sent successfully',
--- src/app/bli-medlem/page.tsx
+++ src/app/bli-medlem/page.tsx
@@ -44,6 +44,7 @@ function BecomeMember() {
   const [hasDiseases, setHasDiseases] = useState(false);
   const [needsGuardian, setNeedsGuardian] = useState(false);
   const [hasFriend, setHasFriend] = useState(false);
+  const [statusMessage, setStatusMessage] = useState('');
   const listOfSports = [
     'Boxning',
     'Fristil brottning',
@@ -73,50 +74,21 @@ function BecomeMember() {
     },
   });
 
+  const formDataObject = form.getValues();
   const { isDirty, isSubmitting, isSubmitSuccessful } = form.formState;
-  const {
-    name,
-    emailAddress,
-    address,
-    postalCode,
-    personnumber,
-    telephone,
-    gender,
-    sports,
-    diseases,
-    trainingFrequency,
-    discount,
-    comments,
-    guardianName,
-    guardianTelephone,
-    friendReferal,
-    friendsName,
-  } = form.getValues();
 
   const postEmail = async () => {
     const response = await fetch('/api/emails', {
       method: 'POST',
-      body: JSON.stringify({
-        email: emailAddress,
-        name: name,
-        address,
-      }),
+      body: JSON.stringify(formDataObject),
     });
     const data = await response.json();
-    console.log('data :', data);
+    setStatusMessage(data.message);
   };
 
-  const handleSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log('values', { values });
+  const onSubmit = () => {
     postEmail();
   };
-
-  useEffect(() => {
-    if (isSubmitSuccessful) {
-      console.log('Form submitted successfully');
-      form.reset();
-    }
-  }, [form, isSubmitSuccessful, isSubmitting]);
 
   const handlePersonNumber = (value: string) => {
     form.setValue('personnumber', value);
@@ -124,94 +96,98 @@ function BecomeMember() {
     setNeedsGuardian(!isAbove18);
   };
 
+  useEffect(() => {
+    if (isSubmitSuccessful) {
+      form.reset();
+    }
+  }, [form, isSubmitSuccessful, isSubmitting]);
+
   return (
-    <>
-      <main className="flex min-h-screen flex-col items-center justify-between px-3 pb-6">
-        <Form {...form}>
-          <Card className="max-w-xl w-full md:min-w-[70%] xl:min-w-[60rem]">
-            <CardHeader>
-              <CardTitle>Registrera dig</CardTitle>
-              <CardDescription>Var vänlig fyll i formuläret för att bli medlem</CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(handleSubmit)}>
-                {/* Name Field */}
-                <NameField form={form} />
+    <main className="flex min-h-screen flex-col items-center justify-between px-3 pb-6">
+      <Form {...form}>
+        <Card className="max-w-xl w-full md:min-w-[70%] xl:min-w-[60rem]">
+          <CardHeader>
+            <CardTitle>Registrera dig</CardTitle>
+            <CardDescription>Var vänlig fyll i formuläret för att bli medlem</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
+              {/* Name Field */}
+              <NameField form={form} />
 
-                {/* Personnumber field */}
-                <PersonNumberField handlePersonNumber={handlePersonNumber} form={form} />
+              {/* Personnumber field */}
+              <PersonNumberField handlePersonNumber={handlePersonNumber} form={form} />
 
-                {/* Guardian fields */}
-                {form.watch('personnumber').length > 9 && needsGuardian && (
-                  <>
-                    <GuardianNameField form={form} />
-                    <GuardianTelephoneField form={form} />
-                  </>
-                )}
+              {/* Guardian fields */}
+              {form.watch('personnumber').length > 9 && needsGuardian && (
+                <>
+                  <GuardianNameField form={form} />
+                  <GuardianTelephoneField form={form} />
+                </>
+              )}
 
-                {/* Email address field */}
-                <EmailAdressField form={form} />
+              {/* Email address field */}
+              <EmailAdressField form={form} />
 
-                {/* Telephone field */}
-                <TelephoneField form={form} />
+              {/* Telephone field */}
+              <TelephoneField form={form} />
 
-                {/* Address field */}
-                <AddressField form={form} />
+              {/* Address field */}
+              <AddressField form={form} />
 
-                {/* Postal code field */}
-                <PostalCodeField form={form} />
+              {/* Postal code field */}
+              <PostalCodeField form={form} />
 
-                {/* Gender field */}
-                <GenderField form={form} />
+              {/* Gender field */}
+              <GenderField form={form} />
 
-                {/* Sports field */}
-                <SportsField form={form} sports={listOfSports} />
+              {/* Sports field */}
+              <SportsField form={form} sports={listOfSports} />
 
-                {/* Training Frequency */}
-                <TrainingFrequencyField form={form} />
+              {/* Training Frequency */}
+              <TrainingFrequencyField form={form} />
 
-                {/* FriendReferal Checkbox */}
-                <FriendReferalField form={form} setHasFriend={setHasFriend} />
-                {/* FriendReferal Field */}
-                {hasFriend && <FriendNameFields form={form} />}
+              {/* FriendReferal Checkbox */}
+              <FriendReferalField form={form} setHasFriend={setHasFriend} />
+              {/* FriendReferal Field */}
+              {hasFriend && <FriendNameFields form={form} />}
 
-                {/* Discount Field */}
-                <DiscountField form={form} />
+              {/* Discount Field */}
+              <DiscountField form={form} />
 
-                {/* HasDisease checkbox */}
-                <HasDiseaseCheckbox
-                  hasDiseases={hasDiseases}
-                  setHasDiseases={(checked: boolean) => setHasDiseases(checked)}
-                />
-                {/* Disease field */}
-                {hasDiseases && <DiseaseField form={form} />}
+              {/* HasDisease checkbox */}
+              <HasDiseaseCheckbox
+                hasDiseases={hasDiseases}
+                setHasDiseases={(checked: boolean) => setHasDiseases(checked)}
+              />
+              {/* Disease field */}
+              {hasDiseases && <DiseaseField form={form} />}
 
-                {/* Comments field */}
-                <CommentsCheckbox hasComments={hasComments} setHasComments={setHasComments} />
-                {hasComments && <CommentsForm form={form} />}
+              {/* Comments field */}
+              <CommentsCheckbox hasComments={hasComments} setHasComments={setHasComments} />
+              {hasComments && <CommentsForm form={form} />}
 
-                <CardFooter className="flex md:justify-end">
-                  <Button
-                    disabled={!isDirty || isSubmitting}
-                    className="min-w-fit w-full md:w-auto"
-                    type="submit"
-                  >
-                    {isSubmitting ? (
-                      <>
-                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                        Skickar..
-                      </>
-                    ) : (
-                      'Skicka in'
-                    )}
-                  </Button>
-                </CardFooter>
-              </form>
-            </CardContent>
-          </Card>
-        </Form>
-      </main>
-    </>
+              <CardFooter className="flex md:justify-end">
+                <Button
+                  disabled={!isDirty || isSubmitting}
+                  className="min-w-fit w-full md:w-auto"
+                  type="submit"
+                >
+                  {isSubmitting ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      Skickar..
+                    </>
+                  ) : (
+                    'Skicka in'
+                  )}
+                </Button>
+              </CardFooter>
+            </form>
+          </CardContent>
+        </Card>
+      </Form>
+    </main>
   );
 }
 
