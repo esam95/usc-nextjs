d0b7297f6063e21f1e6255497572e7b3e5285076
Esam Gelani <esam_95@hotmail.se>
2024-07-12 15:42:57 +0200

    feat: replace MUI components with shadcn components in schedule page and q&a page

--- src/app/om-oss/page.tsx
+++ src/app/om-oss/page.tsx
@@ -1,11 +1,4 @@
 import React from "react";
-import { 
-  Container, 
-  Box, 
-  Typography, 
-  Grid, 
-  CardMedia, 
-} from "@mui/material";
 
 import {
   Card,
@@ -13,75 +6,51 @@ import {
   CardTitle,
   CardDescription,
   CardContent,
-  CardFooter,
 } from '@/components/shadcn/card';
-
+import Image from 'next/image'
 
 const AboutUs = () => {
   return (
-    <Card>
-        <Typography variant="h4" gutterBottom>
-          Om oss
-        </Typography>
+    <>
+      <h1>Om oss</h1>
+      <Card>
         <CardHeader>
-              <CardTitle>Välkommen till <span style={{ fontWeight: "bold" }}>
-                    Upprustningen Sports Club!
-                  </span></CardTitle>
-              <CardDescription>Grundades år 2023, vårt uppdrag är att stärka individer 
-                  genom högkvalitativ träning, och främja både fysisk och 
-                  mental utveckling.</CardDescription>
-            </CardHeader>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={6}>
-            <Card>
-              <CardContent>
-                
-                <Typography variant="h6" gutterBottom>
-                  Vår filosofi
-                </Typography>
-                <Typography variant="body1" paragraph>
-                  På Upprustningen Sports Club tror vi på en helhetssyn på 
-                  kampsport. Våra träningsprogram är utformade för att 
-                  förbättra färdigheter, bygga karaktär och skapa en stödjande 
-                  gemenskap.
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Erbjudna program
-                </Typography>
-                <Typography variant="body1" paragraph>
-                  - <b>Boxing:</b> Från nybörjare till avancerade nivåer.
-                  <br />
-                  - <b>MMA:</b> Omfattande träning för alla färdighetsnivåer.
-                  <br />
-                  - <b>Submission wrestling:</b> En mix av stående och liggande 
-                  brottning.<br />
-                  - <b>Fristils brottning:</b> Lärorika träningspass med 
-                  erfarna tränare.
-                </Typography>
-                <Typography variant="h6" gutterBottom>
-                  Våra Tränare
-                </Typography>
-                <Typography variant="body1" paragraph>
-                  Vårt erfarna tränarteam inkluderar erfarna proffs med 
-                  imponerande bakgrunder inom olika kampsportsdiscipliner. 
-                  De är dedikerade till att hjälpa dig att nå din personliga 
-                  bästa nivå.
-                </Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Card>
-              <CardMedia
-                component="img"
-                height="300"
-                image="/Logga cirkulär.png"
-                alt="Club Image"
-              />
-            </Card>
-          </Grid>
-        </Grid>
+          <CardTitle>Välkommen till <span className="font-bold">Upprustningen Sports Club!</span></CardTitle>
+          <CardDescription>Grundades år 2023, vårt uppdrag är att stärka individer 
+              genom högkvalitativ träning, och främja både fysisk och 
+              mental utveckling.</CardDescription>
+        </CardHeader>
+        <CardContent className="flex">
+          <section className="flex-1">
+          <h4>Vår filosofi</h4>
+          <p >
+            På Upprustningen Sports Club tror vi på en helhetssyn på kampsport. Våra träningsprogram är utformade för att förbättra färdigheter, bygga karaktär och skapa en stödjande gemenskap.
+          </p><br/>
+          
+          <h4 >Erbjudna program</h4>
+          <p>
+            - <b>Boxing:</b> Från nybörjare till avancerade nivåer.<br />
+            - <b>MMA:</b> Omfattande träning för alla färdighetsnivåer.<br />
+            - <b>Submission wrestling:</b> En mix av stående och liggande brottning.<br />
+            - <b>Fristils brottning:</b> Lärorika träningspass med erfarna tränare.
+          </p><br/>
+          
+          <h4>Våra Tränare</h4>
+          <p >
+            Vårt erfarna tränarteam inkluderar erfarna proffs med imponerande bakgrunder inom olika kampsportsdiscipliner. De är dedikerade till att hjälpa dig att nå din personliga bästa nivå.
+          </p>
+          </section>
+          <section className="flex-1 flex justify-center">
+            <Image
+              src="/Logga cirkulär.png"
+              width={500}
+              height={500}
+              alt="Club Image"
+            />
+          </section>
+        </CardContent>
       </Card>
+    </>
   );
 };
 
