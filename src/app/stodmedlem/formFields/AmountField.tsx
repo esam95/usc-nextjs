b971f5f16ea534416c7058b748c65e3073f78c9a
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-22 15:05:46 +0200

    style: remove arrow buttons for setting numbers

--- src/app/stodmedlem/formFields/AmountField.tsx
+++ src/app/stodmedlem/formFields/AmountField.tsx
@@ -2,18 +2,24 @@ import { FormField, FormItem, FormLabel, FormControl, FormMessage } from '@/comp
 import { Input } from '@/components/shadcn/input';
 import React from 'react';
 import { FormFieldProps } from '../page';
+import '../styles.css';
 
 function AmountField({ form }: FormFieldProps) {
   return (
     <FormField
-      name="amount"
+      name='amount'
       control={form.control}
       render={({ field }) => {
         return (
           <FormItem>
-            <FormLabel className="text-md">Stödmängd</FormLabel>
+            <FormLabel className='text-md'>Stödmängd</FormLabel>
             <FormControl>
-              <Input type='number' placeholder="Fyll i önskad mängd *" {...field} />
+              <Input
+                type='number'
+                placeholder='Fyll i ett valfritt belopp *'
+                className='hide-arrows'
+                {...field}
+              />
             </FormControl>
             <FormMessage />
           </FormItem>
