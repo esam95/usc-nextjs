541e74a3866cf29cd61b65ad9703ef6f5745e13c
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-23 11:37:58 +0200

    style: fix layout and add toaster

--- src/app/kontakt/page.tsx
+++ src/app/kontakt/page.tsx
@@ -1,15 +1,24 @@
-"use client"
-import { useEffect } from "react"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm, UseFormReturn } from "react-hook-form"
-import { z } from "zod"
-import { Button } from "@/components/shadcn/button"
-import { Form} from "@/components/shadcn/form"
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/shadcn/card';
-import { formSchema } from "./schema/formSchema"
-import NameField from "./formFields/NameField"
-import MessageField from "./formFields/MessageField"
-import EmailAddressField from "./formFields/EmailAddressField"
+'use client';
+import { useEffect } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FieldErrors, useForm, UseFormReturn } from 'react-hook-form';
+import { z } from 'zod';
+import { Button } from '@/components/shadcn/button';
+import { Form } from '@/components/shadcn/form';
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from '@/components/shadcn/card';
+import { formSchema } from './schema/formSchema';
+import NameField from './formFields/NameField';
+import MessageField from './formFields/MessageField';
+import EmailAddressField from './formFields/EmailAddressField';
+import { Loader2 } from 'lucide-react';
+import { useToast } from '@/components/shadcn/use-toast';
 
 export type FormFieldProps = {
   form: UseFormReturn<z.infer<typeof formSchema>>;
@@ -19,72 +28,116 @@ export default function Contact() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      fullName: "",
-      emailAddress: "",
-      message: "",
+      fullName: '',
+      emailAddress: '',
+      message: '',
     },
-  })
-
+  });
+  const { toast } = useToast();
   const { isDirty, isSubmitting, isSubmitSuccessful } = form.formState;
-  const { fullName, emailAddress, message } = form.getValues()
+  const { fullName, emailAddress, message } = form.getValues();
 
   const postEmail = async () => {
-    const response = await fetch('/api/contactEmail', {
-      method: 'POST',
-      body: JSON.stringify({
-        fullName,
-        emailAddress,
-        message,
-      }),
-    });
-    const data = await response.json();
-    console.log('data :', data);
+    try {
+      const response = await fetch('/api/contactEmail', {
+        method: 'POST',
+        body: JSON.stringify({
+          fullName,
+          emailAddress,
+          message,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Check the api endpoint, status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      if (data.success) {
+        toast({
+          title: 'Tack för din registrering!',
+          description: 'Ditt meddelande har skickats',
+          variant: 'success',
+        });
+      } else {
+        throw new Error(data.message || 'Unknown error');
+      }
+    } catch (error) {
+      toast({
+        title: 'Något blev fel!',
+        description: 'Vi kunde tyvärr inte skicka ditt meddelande',
+        variant: 'destructive',
+      });
+      console.error('Error:', error);
+    }
   };
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log('values', { values });
-    postEmail();
-  }
+  const onSubmit = async () => {
+    await postEmail();
+  };
+
+  const onError = (errors: FieldErrors) => {
+    if (errors) {
+      toast({
+        title: 'Något blev fel!',
+        description: 'Vänligen fyll i alla obligatoriska fält',
+        variant: 'destructive',
+      });
+    }
+  };
 
   useEffect(() => {
     if (isSubmitSuccessful) {
-      console.log('Form submitted successfully');
       form.reset();
     }
   }, [form, isSubmitSuccessful, isSubmitting]);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between px-3 pb-6">
+    <div className='flex min-h-screen min-w-80 w-full flex-col items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8'>
+      <div className='w-full max-w-3xl mb-8'>
+        <h1 className='text-center font-extrabold tracking-tight lg:text-5xl text-gray-700'>Kontakta oss</h1>
+      </div>
+
       <Form {...form}>
-        <Card className="max-w-xl w-full md:min-w-[70%] xl:min-w-[60rem]">
-          <CardHeader>
-            <CardTitle>Kontakta oss</CardTitle>
-            <CardDescription>Vi besvarar alla dina frågor inom 24 timmar.</CardDescription>
+        <Card className='w-full max-w-3xl bg-white shadow-lg rounded-lg'>
+          <CardHeader className='bg-primary text-primary-foreground rounded-t-lg p-6'>
+            <CardTitle className='text-2xl font-bold'>Har du funderingar?</CardTitle>
+            <CardDescription className='text-primary-foreground/70'>
+              Vi besvarar alla dina frågor inom 24 timmar.
+            </CardDescription>
           </CardHeader>
-          <CardContent>
-            <form className="flex flex-col gap-5" onSubmit={form.handleSubmit(onSubmit)}>
+          <CardContent className='p-6'>
+            <form className='flex flex-col gap-5' onSubmit={form.handleSubmit(onSubmit, onError)}>
               {/* Name Field */}
               <NameField form={form} />
-              
+
               {/* Emailadress Field */}
               <EmailAddressField form={form} />
 
               {/* Message Field */}
               <MessageField form={form} />
 
-              <CardFooter className="flex md:justify-end">
+              <CardFooter className='flex justify-end p-6'>
                 <Button
                   disabled={!isDirty || isSubmitting}
-                  className="min-w-fit w-full md:w-auto"
-                  type="submit"
+                  className='w-full md:w-auto bg-primary text-primary-foreground hover:bg-primary/90'
+                  type='submit'
                 >
-                  Skicka in
+                  {form.formState.isSubmitting ? (
+                    <>
+                      <Loader2 className='mr-2 h-4 w-4 animate-spin' />
+                      Skickar..
+                    </>
+                  ) : (
+                    'Skicka in'
+                  )}
                 </Button>
               </CardFooter>
             </form>
           </CardContent>
         </Card>
       </Form>
-    </main>
-  )
+    </div>
+  );
 }
