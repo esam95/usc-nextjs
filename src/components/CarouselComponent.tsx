5a47c652445fc5cff9358d53bef2db9e62fd5f77
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-24 15:27:10 +0200

    feat: add image and delaytime prop for Carousel

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -1,54 +1,3 @@
-// import * as React from 'react';
-// import Autoplay from 'embla-carousel-autoplay';
-
-// import {
-//   Carousel,
-//   CarouselContent,
-//   CarouselItem,
-//   CarouselNext,
-//   CarouselPrevious,
-// } from '@/components/shadcn/carousel';
-// import { Card, CardContent } from './shadcn/card';
-// import { StaticImageData } from 'next/image';
-// import Image from 'next/image';
-
-// type Props = {
-//   images?: StaticImageData[];
-// };
-
-// export function CarouselComponent({ images }: Props) {
-//   const plugin = React.useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
-
-//   return (
-//     <Carousel
-//       plugins={[plugin.current]}
-//       className='w-full max-w-xs'
-//       onMouseEnter={plugin.current.stop}
-//       onMouseLeave={plugin.current.reset}
-//     >
-//       <CarouselContent>
-//         {images?.map((image, index: number) => (
-//           <CarouselItem key={index}>
-//             <div className='p-1'>
-//               <Card>
-//                 <CardContent className='flex aspect-square items-center justify-center p-6'>
-//                   {image ? (
-//                     <Image src={image} className='text-4xl font-semibold' alt='' />
-//                   ) : (
-//                     <span className='text-4xl font-semibold'>{index + 1}</span>
-//                   )}
-//                 </CardContent>
-//               </Card>
-//             </div>
-//           </CarouselItem>
-//         ))}
-//       </CarouselContent>
-//       <CarouselPrevious />
-//       <CarouselNext />
-//     </Carousel>
-//   );
-// }
-
 import Autoplay from 'embla-carousel-autoplay';
 import {
   Carousel,
@@ -64,10 +13,11 @@ import { useRef } from 'react';
 
 type Props = {
   images?: StaticImageData[];
+  delayTime?: number;
 };
 
-export function CarouselComponent({ images }: Props) {
-  const plugin = useRef(Autoplay({ delay: 2000, stopOnInteraction: true }));
+export function CarouselComponent({ images, delayTime }: Props) {
+  const plugin = useRef(Autoplay({ delay: delayTime || 2500, stopOnInteraction: true }));
 
   return (
     <Carousel
@@ -79,23 +29,29 @@ export function CarouselComponent({ images }: Props) {
       <CarouselContent>
         {images && images.length > 0
           ? images.map((image, index: number) => (
-              <CarouselItem key={index}>
+              <CarouselItem key={index} className='fixed-size'>
                 <div className='p-1'>
                   <Card>
-                    <CardContent className='flex aspect-square items-center justify-center p-6'>
+                    <CardContent className='flex aspect-square items-center justify-center p-0'>
                       <Image
                         src={image}
                         alt={`Carousel image ${index + 1}`}
                         sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+                        className='opacity-75'
+                        style={{
+                          objectFit: 'cover',
+                          width: '100%',
+                          height: '100%',
+                        }}
                       />
                     </CardContent>
                   </Card>
                 </div>
               </CarouselItem>
             ))
-          : //Fallback for no image passed as prop
+          : // Fallback for no image passed as prop
             Array.from({ length: 5 }).map((_, index: number) => (
-              <CarouselItem key={index}>
+              <CarouselItem key={index} className='fixed-size'>
                 <div className='p-1'>
                   <Card>
                     <CardContent className='flex aspect-square items-center justify-center p-6'>
