8890460e780f8a71fb7766a0e888d25aa545e55a
Abdul-Muqet Khan <abdul.khan@doktor.se>
2024-07-24 21:16:03 +0200

    style: add hover animation class

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,16 +1,44 @@
+'use client';
 import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image';
 import { MenuWizard } from './MenuWizard';
 import { Button } from './shadcn/button';
-
+import { usePathname } from 'next/navigation';
 type Props = {
   title: string;
   imageSrc: string | StaticImageData;
 };
 
 export function Navbar({ imageSrc, title }: Props) {
+  const pathname = usePathname();
+  const links = [
+    { href: '/', label: 'Hem' },
+    { href: '/om-oss', label: 'Om oss' },
+    { href: '/schema', label: 'Schema' },
+    { href: '/kontakta-oss', label: 'Kontakt' },
+    { href: '/Q&A', label: 'FAQ' },
+  ];
+
+  const isActive = (href: string) => {
+    return pathname === href;
+  };
+  console.log(pathname);
+
+  const navLinks = links.map((link) => (
+    <Link
+      key={link.href}
+      href={link.href}
+      className={`
+        ${isActive(link.href) ? 'text-primary' : 'text-secondary-foreground'}
+        font-medium text-md transition-colors hover:text-secondary hover:underline-offset-4 xl:text-lg dark:hover:text-gray-50 hover-animation`}
+      prefetch={false}
+    >
+      {link.label}
+    </Link>
+  ));
+
   return (
-    <nav className='w-full fixed top-0 left-0 z-20  bg-background/20 dark:bg-opacity-50'>
+    <nav className='w-full fixed top-0 left-0 z-20  bg-background/40 dark:bg-[#0000008C]'>
       <div className='container pl-5 flex h-32 items-center justify-between'>
         <Link href='/' className='flex items-center gap-4 text-lg font-semibold' prefetch={false}>
           <Image src={imageSrc} alt='logo' width={60} height={60} />
@@ -19,48 +47,12 @@ export function Navbar({ imageSrc, title }: Props) {
         <div className='lg:hidden'>
           <MenuWizard />
         </div>
-        <div className='hidden lg:flex items-center space-x-4 gap-4'>
-          <Link
-            href='/'
-            className='font-medium text-md text-secondary-foreground transition-colors hover:text-secondary hover:underline hover:underline-offset-4 dark:hover:text-gray-50'
-            prefetch={false}
-          >
-            Hem
-          </Link>
-          <Link
-            href='/om-oss'
-            className='font-medium text-md text-secondary-foreground transition-colors hover:text-secondary hover:underline hover:underline-offset-4 dark:hover:text-gray-50'
-            prefetch={false}
-          >
-            Om oss
-          </Link>
-          <Link
-            href='/schema'
-            className='font-medium text-md text-secondary-foreground transition-colors hover:text-secondary hover:underline hover:underline-offset-4 dark:hover:text-gray-50'
-            prefetch={false}
-          >
-            Schema
-          </Link>
-          <Link
-            href='/kontakta-oss'
-            className='font-medium text-md text-secondary-foreground transition-colors hover:text-secondary hover:underline hover:underline-offset-4 dark:hover:text-gray-50'
-            prefetch={false}
-          >
-            Kontakt
-          </Link>
-          <Link
-            href='/Q&A'
-            className='font-medium text-md text-secondary-foreground transition-colors hover:text-secondary hover:underline hover:underline-offset-2 dark:hover:text-gray-50'
-            prefetch={false}
-          >
-            FAQ
-          </Link>
-        </div>
+        <div className='hidden lg:flex items-center space-x-4 gap-4'>{navLinks}</div>
         <div className='hidden lg:flex items-center'>
           <Link href='/bli-medlem' className='font-medium transition-colors' prefetch={false}>
             <Button
               variant='default'
-              className='text-primary-foreground hover:text-primary hover:bg-slate-200'
+              className='lg:text-md text-primary-foreground hover:text-primary hover:bg-slate-200 xl:h-11 rounded-md px-8'
             >
               Bli medlem
             </Button>
