c175c6ccdbb50aa84703a4f08e3f08d6583dd0f7
Esam Gelani <esam_95@hotmail.se>
2025-01-06 11:59:47 +0100

    Chore: Change schedule and add sports

--- src/functions/pickColor.ts
+++ src/functions/pickColor.ts
@@ -1,9 +1,10 @@
 export const pickColor = (activity: string | null) => {
   return activity === 'Boxning' ? 'bg-orange-700'
   : activity === 'Fys' ? 'bg-stone-700'
-  : activity === 'Grappling' ? 'bg-green-700'
+  : activity === 'BJJ' ? 'bg-emerald-700'
   : activity === 'Brottning' ? 'bg-slate-700'
   : activity === 'Brottning\n(Öppen matta)' ? 'bg-slate-700'
   : activity === 'Boxning\n(Kvinnor)' ? 'bg-pink-700'
+  : activity === 'Brottning\n(Barn)' ? 'bg-blue-700'
   : null
 }
--- src/types/shedule.ts
+++ src/types/shedule.ts
@@ -12,11 +12,11 @@ export const timeSlots: string[] = [
 export const days: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
 
 export const schedule: Schedule = {
-  'Måndag': { '18:30 - 20:30': 'Fys' },
-  'Tisdag': { '18:30 - 20:00': 'Brottning' },
-  'Onsdag': { '17:30 - 19:00': 'Boxning\n(Kvinnor)', '19:00 - 20:00': 'Brottning\n(Barn)', '20:00 - 21:30': 'Boxning' },
-  'Torsdag': { '18:30 - 20:00': 'Brottning' },
-  'Fredag': { '18:30 - 20:00': 'Brottning\n(Öppen matta)' },
+  'Måndag': { '18:30 - 20:00': 'Fys' },
+  'Tisdag': { '18:30 - 20:30': 'Brottning' },
+  'Onsdag': { '17:30 - 19:00': 'Boxning\n(Kvinnor)', '19:00 - 20:00': 'Brottning\n(Barn)', '20:00 - 21:00': 'Boxning' },
+  'Torsdag': { '18:30 - 19:30': 'BJJ', '19:30 - 21:00': 'Brottning' },
+  'Fredag': { '18:30 - 20:30': 'Brottning\n(Öppen matta)' },
   'Lördag': { '11:00 - 12:30': 'Boxning\n(Kvinnor)', '13:00 - 14:00': 'Brottning\n(Barn)' },
-  'Söndag': { '13:00 - 15:00': 'Boxning' },
+  'Söndag': { '11:00 - 12:30': 'BJJ', '13:00 - 14:30': 'Boxning' },
 };
