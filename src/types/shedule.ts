1a8a6e35ee778bb236f15e1cb4fb72e126084044
Esam Gelani <esam_95@hotmail.se>
2025-05-05 13:17:27 +0200

    Chore: Update schedule

--- src/types/shedule.ts
+++ src/types/shedule.ts
@@ -12,11 +12,11 @@ export const timeSlots: string[] = [
 export const days: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag'];
 
 export const schedule: Schedule = {
-  'Måndag':   { '17:30 - 19:00': 'Fys\n(Kvinnor)', '19:00 - 20:30': 'Fys' },
+  'Måndag':   { '17:30 - 19:00': 'Fys\n(Kvinnor)', '19:00 - 21:00': 'Fys' },
   'Tisdag':   { '18:00 - 19:30': 'BJJ', '19:30 - 21:00': 'Brottning' },
   'Onsdag':   { '17:30 - 19:00': 'Boxning\n(Kvinnor)', '19:00 - 20:00': 'Brottning\n(Barn)', '20:00 - 21:00': 'Boxning' },
   'Torsdag':  { '19:00 - 21:00': 'Brottning' },
   'Fredag':   { '18:30 - 20:30': 'Brottning\n(Öppen matta)' },
-  'Lördag':   { '11:00 - 12:30': 'Boxning\n(Kvinnor)', '13:00 - 14:00': 'Brottning\n(Barn)' },
+  'Lördag':   { '11:00 - 12:30': 'Boxning\n(Kvinnor)', '13:00 - 14:30': 'Brottning\n(Barn)' },
   'Söndag':   { '11:00 - 12:30': 'BJJ', '13:00 - 14:30': 'Boxning' },
 };
