3d100c8e923ecb959fd3893d1b21437fae8d6d5f
DittGitHubAnvändarnamn <din@email.com>
2025-12-15 17:53:29 +0100

    Feat: Add gymnastics

--- src/constants/sports.ts
+++ src/constants/sports.ts
@@ -1,5 +1,5 @@
 export const listOfSports: ListOfSports = {
     men: ['Boxning', 'Olympisk brottning', 'BJJ', 'Fys & konditionsträningar'],
     women: ['Boxning', 'Fys & konditionsträningar'],
-    children: ['Olympisk brottning'],
+    children: ['Olympisk brottning', 'Gymnastik(Endast för tjejer mellan 4-14 år)'],
   }
