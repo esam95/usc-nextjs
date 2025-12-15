af8d71b86d20e7f8623f84c42ceeec07b53ad934
DittGitHubAnvändarnamn <din@email.com>
2025-12-15 17:54:07 +0100

    Feat: Add MMA

--- src/constants/sports.ts
+++ src/constants/sports.ts
@@ -1,5 +1,5 @@
 export const listOfSports: ListOfSports = {
-    men: ['Boxning', 'Olympisk brottning', 'BJJ', 'Fys & konditionsträningar'],
+    men: ['Boxning', 'Olympisk brottning', 'BJJ', 'MMA', 'Fys & konditionsträningar'],
     women: ['Boxning', 'Fys & konditionsträningar'],
     children: ['Olympisk brottning', 'Gymnastik(Endast för tjejer mellan 4-14 år)'],
   }
