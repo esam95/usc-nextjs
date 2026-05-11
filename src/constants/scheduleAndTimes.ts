8ab3d96f2949d36be5a19556d3d7c7bb7b26834b
DittGitHubAnvändarnamn <din@email.com>
2026-05-11 16:50:16 +0200

    Chore: Update schedule

--- src/constants/scheduleAndTimes.ts
+++ src/constants/scheduleAndTimes.ts
@@ -7,16 +7,16 @@ export const days: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag
 
 export const schedule: Schedule = {
   'Måndag': {
-    '17:00 - 18:00': 'Barn tävlingsgruppen',
-    '18:00 - 19:00': 'Boxning\n(MMA)',
+    '18:00 - 19:00': 'Barn tävlingsgruppen\n& nybörjargruppen',
     '19:00 - 21:00': 'Brottarfys',
   },
   'Tisdag': {
-    '18:00 - 19:30': 'BJJ\n(MMA)',
-    '19:30 - 21:20': 'Brottning\n(Greco)',
+    '17:00 - 18:00': 'Boxning\n(Tjejer)',
+    '18:00 - 19:30': 'BJJ',
+    '19:30 - 21:30': 'Brottning\n(Greco)',
   },
   'Onsdag': {
-    '17:30 - 19:00': 'Boxning\n(Tjejer)',
+    '18:00 - 19:00': 'Barn nybörjargruppen',
     '19:00 - 20:30': 'Barn tävlingsgruppen',
     '20:30 - 22:00': 'Öppenmatta\n(BJJ & Brottning)',
   },
@@ -24,38 +24,36 @@ export const schedule: Schedule = {
     '19:00 - 21:00': 'Brottning\n(Greco)',
   },
   'Fredag': {
-    '17:30 - 19:00': 'BJJ\n(MMA)',
+    '17:30 - 19:00': 'BJJ',
     '19:00 - 21:00': 'Brottning\n(Greco)',
   },
   'Lördag': {
+    '09:00 - 09:50': 'Brottarlekis',
     '10:00 - 10:50': 'Brottarlekis',
     '11:00 - 12:30': 'Boxning\n(Tjejer)',
-    '12:30 - 13:30': 'Boxning\n(Sparring)',
     '13:30 - 15:30': 'Barn tävlingsgruppen',
     '15:30 - 17:00': 'Barn nybörjargruppen',
   },
   'Söndag': {
-    '10:00 - 11:00': 'Fys\n(Tjejer)',
     '11:00 - 12:30': 'BJJ',
-    '12:30 - 14:00': 'MMA',
-    '14:00 - 15:00': 'Barn nybörjargruppen',
-    '15:00 - 16:00': 'Gymnastik',
+    '12:30 - 14:00': 'Öppenmatta',
+    '14:30 - 15:30': 'Gymnastik',
   },
 };
 
 
 export const workoutDays: WorkoutDays = {
   man: {
-    boxning: [3, 0],
-    olympiskBrottning: [2, 4, 5],
-    bjj: [2, 0],
-    fysOchKondition: [1],
+    boxning: [2, 6],          // Tisdag(tjejer men låt vara), Lördag - egentligen bara tjejer nu?
+    olympiskBrottning: [2, 4, 5, 6], // Tisdag, Torsdag, Fredag, Lördag
+    bjj: [2, 5, 0],           // Tisdag, Fredag, Söndag
+    fysOchKondition: [1],     // Måndag (Brottarfys)
   },
   woman: {
-    boxning: [6],
-    fysOchKondition: [1],
+    boxning: [2, 6],          // Tisdag, Lördag
+    fysOchKondition: [],
   },
   child: {
-    olympiskBrottning: [4, 6],
+    olympiskBrottning: [6, 0], // Lördag, Söndag
   }
 }
