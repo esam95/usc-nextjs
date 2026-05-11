7ddbdcce1f25cd3c156e7ed7cab7419aad05015a
DittGitHubAnvändarnamn <din@email.com>
2026-05-11 16:09:52 +0200

    Feat: Add sitemap

---
+++ src/app/sitemap.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    { url: 'https://zenithkampsport.se', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/bli-medlem', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/kontakt', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/medlemskap', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/om-oss', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/provtrana', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/Q&A', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/schema', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/stodmedlem', lastModified: new Date() },
+    { url: 'https://zenithkampsport.se/villkor', lastModified: new Date() },
+  ]
+}
